c5c7789e3a5a906bf98c525518981572aa57cbc3
chanchal26 <chanchal.mch2613@gmail.com>
2024-07-15 14:10:52 +0600

    stats section added

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
+import Stats from './components/Stats';
+import Skills from './components/Skills';
 
 const page = () => {
   return (
     <>
       <Hero />
       <About />
+      <Stats />
+      <Skills />
     </>
   );
 };
